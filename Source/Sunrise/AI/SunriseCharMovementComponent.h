fc8988b62660079fa537fa94312a82f8b0f5507b
Neil Stewart <madcdevelop@gmail.com>
2020-10-05 07:04:43 -0700

    Renamed previous assets from dummy to Follower. Added new class for character movement component. Added new zombie skin.

---
+++ Source/Sunrise/AI/SunriseCharMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "SunriseCharMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUNRISE_API USunriseCharMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
